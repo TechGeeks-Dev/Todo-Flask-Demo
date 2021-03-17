1a8a25cfa19a92e394b723eb8e920f9d6786714e
Rajdeep Malakar <RajdeepM@tgeeks.cf>
2021-03-17 11:46:07 +0530

    Fixed Alert/Popup

--- static/js/test.js
+++ static/js/test.js
@@ -1,1 +1,1 @@
-alert('Hello from MyTodo Demo App. Keep in mind that it is a Demo Public App, so Please do not add any Private/Sensitive ToDo here!')
+console.log('Hello from MyTodo Demo App. Keep in mind that it is a Demo Public App, so Please do not add any Private/Sensitive ToDo here!');
